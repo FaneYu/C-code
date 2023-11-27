b8f3bb264d468452f8df234f1e6c4c7a5f52ac24
FaneYu <705393916@qq.com>
2023-11-27 20:11:05 +0800

    作业

---
+++ text-11-27/text-11-27/text.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//ื๗าต
+//int main()
+//{
+//	printf("     **\n");
+//	printf("     **\n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *\n");
+//	printf("    *  *\n");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     Man\n");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("I  lost  my  cellphone!\n");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 40;
+//	int c = 211;
+//	int num = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+
+
+//int arr(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int num = arr(a, b);
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+
+
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+	scanf("%d", &x);
+	if (x < 0)
+		y = 1;
+	else if (x == 0)
+		y = 0;
+	else
+		y = -1;
+	printf("%d\n", y);
+	return 0;
+}	
